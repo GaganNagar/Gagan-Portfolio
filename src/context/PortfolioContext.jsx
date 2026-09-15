a4583dc3ae2ffe440079e8cdaab953d55d1a689d
Gagan Nagar <gaganngr319@gmail.com>
2026-09-16 02:24:41 +0530

    update project order

--- src/context/PortfolioContext.jsx
+++ src/context/PortfolioContext.jsx
@@ -30,26 +30,8 @@ export const PortfolioProvider = ({ children }) => {
 
   // --- Portfolio Data ---
   const projectsData = [
-  {
+    {
     id: 1,
-    title: "Employee Management System",
-    desc: "A full-stack role-based management dashboard with secure authentication for Admin and Employee roles, featuring task assignment and real-time tracking.",
-    image: "/ems.png",
-    tech: ["React.js", "Context API", "Tailwind CSS", "LocalStorage"],
-    githubLink: "https://github.com/GaganNagar/employee-management-system",
-    liveLink: null
-  },
-  {
-    id: 2,
-    title: "NextUp - Smart Task Manager",
-    desc: "A productivity-focused task manager with drag-and-drop reordering, smart filtering, and local storage persistence for seamless task tracking.",
-    image: "/nextUp.png",
-    tech: ["JavaScript", "HTML5", "CSS3", "Local Storage"],
-    githubLink: "https://github.com/GaganNagar/NextUp-Task",
-    liveLink: "https://nextup-task.vercel.app/"
-  },
-  {
-    id: 3,
     title: "InterviewAI",
     desc: "An AI-powered assistant providing personalized career guidance through REST API integration and dynamic state handling in React.",
     image: "/aiCareer.png",
@@ -58,13 +40,31 @@ export const PortfolioProvider = ({ children }) => {
     liveLink: "https://careerway-ai.vercel.app/"
   },
   {
-    id: 4,
+    id: 2,
+    title: "Employee Management System",
+    desc: "A full-stack role-based management dashboard with secure authentication for Admin and Employee roles, featuring task assignment and real-time tracking.",
+    image: "/ems.png",
+    tech: ["React.js", "Context API", "Tailwind CSS", "LocalStorage"],
+    githubLink: "https://github.com/GaganNagar/employee-management-system",
+    liveLink: null
+  },
+  {
+    id: 3,
     title: "Personal Portfolio Website",
     desc: "A modern, responsive portfolio built to showcase technical skills and projects with dark mode support and smooth animations.",
     image: "/portfolioProject.png",
     tech: ["React.js", "Tailwind CSS", "AOS", "Typed.js"],
     githubLink: "https://github.com/GaganNagar/Gagan-Portfolio-Hackathon",
     liveLink: "https://gagannagar.vercel.app"
+  },
+  {
+    id: 4,
+    title: "NextUp - Smart Task Manager",
+    desc: "A productivity-focused task manager with drag-and-drop reordering, smart filtering, and local storage persistence for seamless task tracking.",
+    image: "/nextUp.png",
+    tech: ["JavaScript", "HTML5", "CSS3", "Local Storage"],
+    githubLink: "https://github.com/GaganNagar/NextUp-Task",
+    liveLink: "https://nextup-task.vercel.app/"
   }
 ];
 
